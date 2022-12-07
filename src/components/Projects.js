29c779114ef3c1e1c68e73c6a4a235b8b1ace4de
Deadpool1 <atwood169@yahoo.com>
2022-12-06 20:12:24 -0600

    added animation and fixed Projects.js

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,10 +1,11 @@
-import { Tab } from "bootstrap";
-import { Container, Row, Col, Nav } from "react-bootstrap";
-import { ProjectCard } from "./ProjectCard";
+import { Container, Row, Col, Nav, Tab} from "react-bootstrap";
+import { ProjectCard } from "./ProjectCard.js";
 import colorSharp2 from "../assets/img/color-sharp2.jpeg";
 import projImg1 from "../assets/img/projImg1.jpeg";
 import projImg2 from "../assets/img/projImg2.jpeg";
 import projImg3 from "../assets/img/projImg3.jpeg";
+import TrackVisibility from 'react-on-screen';
+import 'animate.css';
 
 export const Projects = () => {
 
@@ -29,44 +30,55 @@ export const Projects = () => {
 
     return (
         <section className="project" id="project">
-        <Container>
+          <Container>
             <Row>
-                <Col>
+              <Col size={12}>
+                <TrackVisibility>
+                  {({ isVisible }) =>
+                  <div className={isVisible ? "animate__animated animate__bounce": ""}>
                     <h2>Projects</h2>
-                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.<br></br> Lorem Ipsum has been the industry's standard dummy text.</p>
+                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                    </div>}
+                    </TrackVisibility>
                     <Tab.Container id="projects-tabs" defaultActiveKey="first">
-                    <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
-                    <Nav.Item>
-                      <Nav.Link eventKey="first">Tab 1</Nav.Link>
-                    </Nav.Item>
-                    <Nav.Item>
-                      <Nav.Link eventKey="second">Tab 2</Nav.Link>
-                    </Nav.Item>
-                    <Nav.Item>
-                      <Nav.Link eventKey="third">Tab 3</Nav.Link>
-                    </Nav.Item>
-                  </Nav>
-                  <Tab.Content>
-                      <Tab.Pane eventKey="first">
-                        <Row>
+                      <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
+                        <Nav.Item>
+                          <Nav.Link eventKey="first">Tab 1</Nav.Link>
+                        </Nav.Item>
+                        <Nav.Item>
+                          <Nav.Link eventKey="second">Tab 2</Nav.Link>
+                        </Nav.Item>
+                        <Nav.Item>
+                          <Nav.Link eventKey="third">Tab 3</Nav.Link>
+                        </Nav.Item>
+                      </Nav>
+                      <Tab.Content>
+                        <Tab.Pane eventKey="first">
+                          <Row>
                             {
-                                projects.map((project, index) => {
-                                    return (
-                                        <ProjectCard
-                                        key={index}
-                                        {...project}
-                                        />
-                                    )
-                                })
+                              projects.map((project, index) => {
+                                return (
+                                  <ProjectCard
+                                    key={index}
+                                    {...project}
+                                    />
+                                )
+                              })
                             }
-                        </Row>
-                      </Tab.Pane>
-                  </Tab.Content>
-                  </Tab.Container>
-                </Col>
+                          </Row>
+                        </Tab.Pane>
+                        <Tab.Pane eventKey="section">
+                          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quam, quod neque provident velit, rem explicabo excepturi id illo molestiae blanditiis, eligendi dicta officiis asperiores delectus quasi inventore debitis quo.</p>
+                        </Tab.Pane>
+                        <Tab.Pane eventKey="third">
+                          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quam, quod neque provident velit, rem explicabo excepturi id illo molestiae blanditiis, eligendi dicta officiis asperiores delectus quasi inventore debitis quo.</p>
+                        </Tab.Pane>
+                      </Tab.Content>
+                    </Tab.Container>
+              </Col>
             </Row>
-        </Container>
-        <img className="background-image-right" src={colorSharp2} alt="prop"></img>
+          </Container>
+          <img className="background-image-right" src={colorSharp2} alt="prop"></img>
         </section>
-    )
-}
+      )
+    }
