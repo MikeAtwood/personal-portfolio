23cf91abb9f2e2f2fcae16574bd9c4a0422340d0
Deadpool1 <atwood169@yahoo.com>
2022-12-28 12:58:48 -0600

    added contact and footer

---
+++ src/components/Footer.js
@@ -0,0 +1,27 @@
+import { Container, Row, Col } from "react-bootstrap";
+import logo from "../assets/img/logo.png";
+import navIcon1 from "../assets/img/nav-icon1.png";
+import navIcon2 from "../assets/img/nav-icon2.png";
+import navIcon3 from "../assets/img/nav-icon3.png";
+
+export const Footer = () => {
+    return (
+        <footer className="footer">
+            <Container>
+                <Row className="align-items-center">
+                    <Col>
+                        <img src={logo} alt="Logo" />
+                    </Col>
+                    <Col size={12} sm={6} className="text-center text-sm-end">
+                        <div className="social-icon">
+                            <a href="https://twitter.com/WWarrior57"><img src={navIcon1} alt="twitter" /></a>
+                            <a href="https://www.linkedin.com/in/michael-atwood-b19321238/"><img src={navIcon2} alt="linkedin" /></a>
+                            <a href="https://github.com/MikeAtwood"><img src={navIcon3} alt="facebook" /></a>
+                        </div>
+                        <p>Copyright 2023. All Rights Reserved</p>
+                    </Col>
+                </Row>
+            </Container>
+        </footer>
+    )
+}
