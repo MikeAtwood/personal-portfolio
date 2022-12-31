c58618df2af7b64470968d7dc2d1eab79a4a63a5
Deadpool1 <atwood169@yahoo.com>
2022-12-30 23:08:45 -0600

    updated server

--- server.js
+++ server.js
@@ -14,6 +14,8 @@ console.log(process.env.EMAIL_PASS);
 
 const contactEmail = nodemailer.createTransport({
   service: 'gmail',
+  host: 'smtp.gmail.com',
+  secure: false,
   auth: {
     user: "atwood174@gmail.com",
     pass: "kuxbiyeoblwadddf"
@@ -35,7 +37,7 @@ router.post("/contact", (req, res) => {
   const phone = req.body.phone;
   const mail = {
     from: name,
-    to: "********@gmail.com",
+    to: "atwood174@gmail.com",
     subject: "Contact Form Submission - Portfolio",
     html: `<p>Name: ${name}</p>
            <p>Email: ${email}</p>
