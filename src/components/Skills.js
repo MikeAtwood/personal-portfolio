35166664340f07205d918a4670df2d6a809b50c7
Deadpool1 <atwood169@yahoo.com>
2022-12-30 20:31:38 -0600

    fixed href

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -39,7 +39,7 @@ export const Skills = () => {
                             <h2>
                                 Skills
                             </h2>
-                            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.<br></br> Lorem Ipsum has been the industry's standard dummy text.</p>
+                            <p>Using today's best methods and practices.<br></br> Lorem Ipsum has been the industry's standard dummy text.</p>
                             <Carousel responsive={responsive} Infinite={true} className="skill-slider">
                                 <div className="item">
                                     <img src={meter1} alt="html" /> 
