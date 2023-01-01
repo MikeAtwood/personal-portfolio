093fc2ff336633a7375bb47712559813b89ffcd5
Deadpool1 <atwood169@yahoo.com>
2023-01-01 14:55:28 -0600

    updated contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,25 +3,31 @@ import controllerImg from "../assets/img/controller.png";
 import 'animate.css';
 
 export const Contact = () => {
-  
+
     return (
         <section className="contact" id="connect">
             <Container>
                 <Row className="align-items-center">
                     <Col md={6}>
-                        <img src={controllerImg} alt="Contact Us"/>
+                        <img src={controllerImg} alt="Contact Us" />
                     </Col>
                     <Col md={6}>
                         <h2>Get In Touch</h2>
-                        <form name="contact" method="POST" data-netlify="true" onSubmit="submit">
+                        <form name="contact" method="post">
                             <p>
-                                <label>Name <input type="text" name="name"/>Name</label>
+                                <label htmlFor="name">Name</label> <br />
+                                <input type="text" id="name" name="name" required />
                             </p>
                             <p>
-                                <label>Email <input type="email" name="email"/>Email</label>
+                                <label htmlFor="email">Email</label> <br />
+                                <input type="email" id="email" name="email" required />
                             </p>
                             <p>
-                                <button type="submit">Send</button>
+                                <label htmlFor="message">Message</label> <br />
+                                <textarea id="message" name="message" required></textarea>
+                            </p>
+                            <p>
+                                <input type="submit" value="Submit message" />
                             </p>
                         </form>
                     </Col>
